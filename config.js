6b2f5436d48c992901b95076d9eaf4b1cccd17bc
Vow4excellence <53167685+Vow4excellence@users.noreply.github.com>
2020-06-18 16:26:13 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA812uKdERRsryfye00qWN_XftWZYB1n_Y",
+    authDomain: "bookdonav4.firebaseapp.com",
+    databaseURL: "https://bookdonav4.firebaseio.com",
+    projectId: "bookdonav4",
+    storageBucket: "bookdonav4.appspot.com",
+    messagingSenderId: "546600212456",
+    appId: "1:546600212456:web:77f1a5351f8c421877f65d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
